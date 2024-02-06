7e3a4189c1933c2c64dc09eb7348fa7d329338d5
Mike Myat Min Han <mmhan@indeed.com>
2024-02-06 18:09:31 +0000

    Basic styling set-up

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # allow console locally
+  config.web_console.permissions = '192.168.0.0'
 end
